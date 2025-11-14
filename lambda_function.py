c33188ea4554372479f9e66eaf1c2b3dc0f4009a
Diego O <111875056+RoyalMoon96@users.noreply.github.com>
2025-11-13 19:24:38 -0600

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,42 +1,7 @@
+# lambda_function.py
 import logging
-from ask_sdk_core.skill_builder import SkillBuilder
-
-from handlers import (
-    LaunchRequestHandler,
-    RegistrarUsuarioIntentHandler,
-    AgendarCitaIntentHandler,
-    ConsultarCitasIntentHandler,
-    CancelarCitaIntentHandler,
-    MoverCitaIntentHandler,
-    InfoDoctorIntentHandler,
-    DisponibilidadDoctorIntentHandler,
-    YesIntentHandler,
-    NoIntentHandler,
-    FallbackIntentHandler,
-    SessionEndedRequestHandler,
-    CatchAllExceptionHandler
-)
-
+from handlers import sb  # import SkillBuilder ya configurado
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
-sb = SkillBuilder()
-
-# Request handlers
-sb.add_request_handler(LaunchRequestHandler())
-sb.add_request_handler(RegistrarUsuarioIntentHandler())
-sb.add_request_handler(AgendarCitaIntentHandler())
-sb.add_request_handler(ConsultarCitasIntentHandler())
-sb.add_request_handler(CancelarCitaIntentHandler())
-sb.add_request_handler(MoverCitaIntentHandler())
-sb.add_request_handler(InfoDoctorIntentHandler())
-sb.add_request_handler(DisponibilidadDoctorIntentHandler())
-sb.add_request_handler(YesIntentHandler())
-sb.add_request_handler(NoIntentHandler())
-sb.add_request_handler(FallbackIntentHandler())
-sb.add_request_handler(SessionEndedRequestHandler())
-
-# Exception handler
-sb.add_exception_handler(CatchAllExceptionHandler())
 
 lambda_handler = sb.lambda_handler()
